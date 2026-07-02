4401c1cdc7b8b0564cf486bbc1d33257d9bf70c7
707sss <github707sss@gmail.com>
2026-07-02 14:37:08 +0500

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Suxrob-Mengliyev/', // Mana shu qator mutloq bo'lishi shart!
+})
